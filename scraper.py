d84f983bd147f666fcc323820f1f17db84f47577
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-06 19:17:20 +0800

    scans all links instead of just those inside 'p'

--- scraper.py
+++ scraper.py
@@ -7,6 +7,10 @@ page = requests.get('https://en.wikipedia.org/wiki/Six_Degrees_of_Kevin_Bacon')
 page.raise_for_status()
 soup = BeautifulSoup(page.text, 'lxml')
 
-new_pages = [page['href'] for page in soup.select('p a')]
-new_pages = [wiki_page for wiki_page in new_pages if re.search('\/wiki\/.*', wiki_page)]
-print(new_pages)
+# get all 'a' tags that appear inside a 'p' tag (there are lots of 'a' tags like navbars that I don't care about,
+# but the ones that are actually part of the page's "content" usually apear inside 'p' tags)
+new_pages = [page['href'] for page in soup.select('a[href]')]   #BUG: throws KeyError if there are no 'p a' tags in the soup
+new_pages = [wiki_page for wiki_page in new_pages if search(
+    '^\/wiki\/.*$', wiki_page)]    # only keep wiki_pages that look like '\wiki\<something>' TODO: Make the regex ignore '/wiki/Wikipedia:about' and similar
+for page in new_pages:
+    print(page)

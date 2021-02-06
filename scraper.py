40de1c00edbf1b5967478a532d79e3d5d5c2ed7c
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-06 11:21:03 +0800

    Scraper only gets links to wiki pages

--- scraper.py
+++ scraper.py
@@ -8,4 +8,5 @@ page.raise_for_status()
 soup = BeautifulSoup(page.text, 'lxml')
 
 new_pages = [page['href'] for page in soup.select('p a')]
+new_pages = [wiki_page for wiki_page in new_pages if re.search('\/wiki\/.*', wiki_page)]
 print(new_pages)

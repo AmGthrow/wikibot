0ded9309184871eb7d50c025a2a54feabda6e6ca
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-06 21:28:58 +0800

    Handles no 'href' edge case
    If the wikipage doesn't have any more links to other wikipedia pages,
    selects a new, random wiki article to search instead

--- scraper.py
+++ scraper.py
@@ -17,7 +17,18 @@ def get_soup(wikipage):
     page.raise_for_status()
     return BeautifulSoup(page.text, 'lxml')
 
-    # get all 'a' tags on the page
+    # get all 'a' tags on the page that have an 'href' atrribute
+    new_pages = soup.select('a[href]')
+
+    # Just in case there are no href's, search a random article instead
+    while new_pages == []:
+        # Erases the "page we came from" since we couldn't find any new wikipages there
+        prev_page = None
+
+        # Travel to a random wikipage
+        soup = get_soup('https://en.wikipedia.org/wiki/Special:Random')
+        # Try finding the new_pages from the new wikipage
+        new_pages = soup.select('a[href]')
 
     # BUG: throws KeyError if there are no 'a' tags in the soup
     # This shouldn't be a problem though, since Wikipedia avoids dead pages like this? See: https://en.wikipedia.org/wiki/Category:Dead-end_pages

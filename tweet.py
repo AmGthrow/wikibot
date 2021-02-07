77535a254e062624438b005db5ecba42e73c51b5
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-07 16:05:05 +0800

    Added expected functions(?)

---
+++ tweet.py
@@ -0,0 +1,22 @@
+
+import scraper
+
+def compose_tweet(URL):
+    """Composes a tweet that's ready to be posted in this format
+
+    <NEW TITLE>
+    
+    Previous page: <URL>
+    Current page: <NEW URL>
+
+    Args:
+        URL (string): A URL containing a wikipage whose links we need to search
+    """
+
+def tweet(URL):
+    """does the actual tweeting
+
+    Args:
+        URL (string): A URL containing a wikipage whose links we need to search
+    """
+    pass

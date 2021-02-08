88ecacaec7b9d2a4ffb224a2c1ba81ad4529b888
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2021-02-08 17:58:41 +0800

    trims summary if too long to fit in a tweet

--- tweet.py
+++ tweet.py
@@ -7,6 +7,10 @@ import logging.handlers
 import tweepy
 from os import getenv
 
+# These are Twitter constants that I rely on for when I'm making a tweetable message, mostly for shorten_summary()
+MAX_TWEET_LENGTH = 280
+URL_LENGTH = 23  # All URLs have a fixed length, see number 2 in https://help.twitter.com/en/using-twitter/how-to-tweet-a-link
+
 logger = logging.getLogger(__name__)
 # set log level
 logger.setLevel(logging.INFO)
@@ -28,7 +32,7 @@ access_token = getenv("ACCESS_TOKEN")
 access_token_secret = getenv("ACCESS_TOKEN_SECRET")
 
 
-def compose_tweet(page):
+def compose_tweet(page, fit_tweet_limit=False, erase_prev=True):
     """Composes a tweet that's ready to be posted in this format
 
     <NEW TITLE>
@@ -54,35 +58,83 @@ def compose_tweet(page):
     # Get rid of Wikipedia citations (they look like this[1]) from the summary
     new_summary = re.sub(r'\[\d+\]', '', new_summary)
 
-    message = f"""
-    {new_title}
+    # fit_tweet_limit means you want to keep the message under MAX_TWEET_LENGTH to fit the Twitter character limit
+    if fit_tweet_limit:
+        new_summary = shorten_summary(new_title, new_summary)
+
+    message = f"""{new_title}
 
     From here: {prev_url}
     I got to here: {new_url}
 
     {new_summary}
     """
+    logging.info(f"composed message:\n\n{message}")
 
-    # Overwrite whatever was in prev_page.txt
-    with open('prev_page.txt', 'w') as prevf:
-        prevf.write(new_url)
+    if erase_prev:
+        # Overwrite whatever was in prev_page.txt
+        with open('prev_page.txt', 'w') as prevf:
+            prevf.write(new_url)
 
     return textwrap.dedent(message)
 
 
-def tweet():
-    """does the actual tweeting
+def shorten_summary(title, summary):
+    """Receives a potential tweet which may be longer than MAX_TWEET_LENGTH. If so, it cuts off the summary and adds an ellipsis
 
     Args:
-        URL (string): A URL containing a wikipage whose links we need to search
+        title (str): The title of the potential tweet
+        summary (str): The summary 
+
+    Returns:
+        str: The same summary except cut down to fit MAX_TWEET_LENGTH, ending in '...' if it was trimmed
+    """
+    # chars_remaining tracks how long my summary is allowed to be
+    chars_remaining = MAX_TWEET_LENGTH
+    chars_remaining -= len(title)   # Subtracts the title
+    # Subtracts the two "transformed URL lengths" of prev_page and new_page
+    chars_remaining -= URL_LENGTH * 2
+    # This is just the length of the "from here I got to here" part, including newlines and spaces
+    chars_remaining -= 29
+    # BUG: I have NO idea why but this function just CONSISTENTLY has 2 excess characters so I guess I have to shave those off
+    chars_remaining -= 2 
+
+    # There are some Unicode characters that Twitter gives a weight of 2, not just 1. e.g. Emojis should reduce chars_remaining by 2, not just 1
+    # See this: https://developer.twitter.com/en/docs/counting-characters
+    # So I need to keep track of those letters and subtract the 'extra weight' from chars_remaining. This for loop does exactly that
+
+    # Assuming the summary has no "2-weight characters", the summary can only be as long as this value
+    # I don't bother checking anything past this since those characters have no hope of being included in the tweet anyway, they're 
+    # already outside the range of chars_remaining
+    potential_longest_index = chars_remaining   
+    
+    # The summary right now might be incredibly long, but I only need to check for 2-weight characters within potential_longest_index
+    for s in summary[:potential_longest_index]:
+        code_point = ord(s)
+        if not (0 <= code_point <= 4351 or \
+        8192 <= code_point <= 8205 or \
+        8208 <= code_point <= 8223 or \
+        8242 <= code_point <= 8247):
+            chars_remaining -= 1    # subtract 1 instead of 2 since len(summary) already covers regular chars. I just need to reduce chars_remaining for ever 2-weight char I have
+
+    # If summary is too long to fit into 1 tweet, cut it off and add an ellipsis
+    if len(summary) > chars_remaining:
+        summary = summary[:chars_remaining - 3] # -3 to make room for the ellipsis
+        summary += '...'
+
+        # Add an ellpisis at the end
+
+    return summary
+
+
+def tweet(url):
+    """Connects to the Twitter API and sends out a tweet using compose_tweet(url)
+
+    Args:
+        url (str): The Wikipedia page whose links need to be searched
     """
     # make the actual message to tweet out
-    with open('prev_page.txt', 'r') as prevf:
-        prev_page = prevf.read()
-    message = compose_tweet(prev_page)
-
-    # Make sure message fits the 280 character limit (with room for an ellipsis)
-    message = message[:277] + '...'
+    message = compose_tweet(url, fit_tweet_limit=True)
 
     # tweet it out
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -93,15 +145,9 @@ def tweet():
     api.update_status(message)
 
 
-# logger.info("Started tweet.py driver code")
-# for _ in range(5):
-#     with open('prev_page.txt', 'r') as prevf:
-#         prev_page = prevf.read()
-#     print(compose_tweet(prev_page))
-# # with open('prev_page.txt', 'r') as prevf:
-# #     prev_page = prevf.read()
-# # print(compose_tweet(prev_page))
-# logger.info("Finished tweet.py driver code")
-
 if __name__ == "__main__":
-    tweet()
+    logger.info('Started tweet.py driver code')
+    with open('prev_page.txt', 'r') as prevf:
+        prev_page = prevf.read()
+    tweet(prev_page)
+    logger.info("Finished tweet.py driver code")
